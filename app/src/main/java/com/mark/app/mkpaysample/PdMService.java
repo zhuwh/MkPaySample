c61ebf08dbe90f736971826e8ae2a3c20ca75b29
Mark <zhuwenhui@maikevip.com>
2017-10-07 19:37:46 +0800

    修正的worldpay 工程

--- app/src/main/java/com/mark/app/mkpaysample/PdMService.java
+++ app/src/main/java/com/mark/app/mkpaysample/PdMService.java
@@ -7,6 +7,8 @@ import io.reactivex.Observable;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.POST;
 
 /**
@@ -44,5 +46,14 @@ public interface PdMService {
     @POST("/api/user/pay")
     Observable<MKBaseResultEntity<String>> getPayInfo();
 
+
+    /**
+     * worldPay
+     *
+     * @return
+     */
+    @FormUrlEncoded
+    @POST("/api/user/pay/wp")
+    Observable<MKBaseResultEntity<String>> worldPay(@Field("token") String token);
 
 }
